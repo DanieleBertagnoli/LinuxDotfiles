8d31b47c17bf9b25b3ba8b2e8fb85a951372ccf2
DanieleBertagnoli <danielebbertagnoli@gmail.com>
2024-09-05 23:43:29 +0200

    added waypaper

--- ArchHyprland2/install.sh
+++ ArchHyprland2/install.sh
@@ -8,10 +8,20 @@ sudo pacman -S hyprland alacritty nautilus waybar hyprpaper starship rofi-waylan
 
 sudo pacman -S eza ttf-nerd-fonts-symbols gnome-keyring breeze gtk4 nwg-look
 
-yay -S wlogout ags bibata-cursor-theme
+yay -S wlogout ags bibata-cursor-theme waypaper
 
 cp -r ./CustomConf/config/* ~/.config
 cp ./CustomConf/.bashrc ~/.bashrc
 cp -r Wallpapers/ ~/Pictures
 
 ~/.config/dotfiles-scripts/set_gtk_themes.sh
+
+sudo systemctl enable sddm.service
+
+if [ ! -d /etc/sddm.conf.d/ ]; then
+    sudo mkdir /etc/sddm.conf.d
+    echo "Folder /etc/sddm.conf.d created."
+fi
+
+sudo cp $HOME/.config/sddm/sddm.conf /etc/sddm.conf.d/
+# sudo cp $HOME/.config/sddm/theme.conf /usr/share/sddm/themes/sugar-candy/
