d5e2225b88f7685b8269e185e747cf2675d013e1
DanieleBertagnoli <danielebbertagnoli@gmail.com>
2024-09-05 16:57:47 +0200

    fixed stuff

--- ArchHyprland2/install.sh
+++ ArchHyprland2/install.sh
@@ -1,9 +1,15 @@
 #!/bin/bash 
 
+cd $(dirname $0)
+
 sudo pacman -Syu
 
-sudo pacman -S hyprland alacritty nautilus waybar hyprpaper starship
+sudo pacman -S hyprland alacritty nautilus waybar hyprpaper starship rofi python-pywal
 
-sudo pacman -S eza ttf-nerd-fonts-symbols
+sudo pacman -S eza ttf-nerd-fonts-symbols gnome-keyring
+
+yay -S wlogout ags
 
 cp -r ./CustomConf/configs/* ~/.config
+cp ./CustomConf/.bashrc ~/.bashrc
+cp -r Wallpapers/ ~/Pictures
