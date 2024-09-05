03cb6fd3a02916cbc31c4f42b59566fc377152ea
DanieleBertagnoli <danielebbertagnoli@gmail.com>
2024-09-05 19:10:31 +0200

    added stuff

--- ArchHyprland2/install.sh
+++ ArchHyprland2/install.sh
@@ -4,12 +4,14 @@ cd $(dirname $0)
 
 sudo pacman -Syu
 
-sudo pacman -S hyprland alacritty nautilus waybar hyprpaper starship rofi python-pywal
+sudo pacman -S hyprland alacritty nautilus waybar hyprpaper starship rofi-wayland python-pywal
 
-sudo pacman -S eza ttf-nerd-fonts-symbols gnome-keyring
+sudo pacman -S eza ttf-nerd-fonts-symbols gnome-keyring breeze gtk4 nwg-look
 
-yay -S wlogout ags
+yay -S wlogout ags bibata-cursor-theme
 
-cp -r ./CustomConf/configs/* ~/.config
+cp -r ./CustomConf/config/* ~/.config
 cp ./CustomConf/.bashrc ~/.bashrc
 cp -r Wallpapers/ ~/Pictures
+
+~/.config/dotfiles-scripts/set_gtk_themes.sh
