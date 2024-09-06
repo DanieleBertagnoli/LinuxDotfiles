23d07d4af93669ccb24def3a41238c076e1f4da6
DanieleBertagnoli <danielebbertagnoli@gmail.com>
2024-09-06 08:37:27 +0200

    fixed waybar themes

--- ArchHyprland2/CustomConf/config/waybar/launch.sh
+++ ArchHyprland2/CustomConf/config/waybar/launch.sh
@@ -7,6 +7,4 @@ sleep 0.5
 config_file="$HOME/.config/waybar/themes/white-blur/config"
 style_file="$HOME/.config/waybar/themes/white-blur/style.css"
 
-cat $config_file
-
 waybar -c $config_file -s $style_file &
