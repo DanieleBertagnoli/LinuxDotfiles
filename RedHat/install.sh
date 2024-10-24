689b8f3799fd022a9cbb7e72396a9593d8f598ae
DanieleBertagnoli <62394828+DanieleBertagnoli@users.noreply.github.com>
2024-10-24 12:09:52 +0200

    Update install.sh

--- RedHat/install.sh
+++ RedHat/install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+cd $(dirname $0)
+
 #Installing font
 cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
 
 # Installing Icon theme
 sudo yum install papirus-icon-theme.noarch
+
+# Installing breeze cursor
+sudo yum install breeze-cursor-theme.noarch
 
 # Installing Starship
 curl -sS https://starship.rs/install.sh | sh
@@ -13,4 +18,7 @@ cp -r ./CustomConf/.configs/* ~/.configs
 # Copying .bashrc configurations
 cp -r ./CustomConf/.bash* ~/
 
-cd $(dirname $0)
+# Apply themes
+gsettings set org.gnome.desktop.interface cursor-theme 'breeze_cursors'
+gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+gsettings set org.gnome.desktop.interface gtk-theme 'Adwaita-dark'
