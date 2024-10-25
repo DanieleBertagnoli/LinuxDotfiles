554ed34c0bd5c5eaf9af7badc33289cdcd571fe5
DanieleBertagnoli <62394828+DanieleBertagnoli@users.noreply.github.com>
2024-10-25 11:39:12 +0200

    Update install.sh

--- RedHat/install.sh
+++ RedHat/install.sh
@@ -1,11 +1,40 @@
 #!/bin/bash
 
-cd $(dirname $0)
+#             .MMM..:MMMMMMM               
+#           MMMMMMMMMMMMMMMMMM            
+#           MMMMMMMMMMMMMMMMMMMM.         
+#          MMMMMMMMMMMMMMMMMMMMMM         
+#         ,MMMMMMMMMMMMMMMMMMMMMM:        
+#         MMMMMMMMMMMMMMMMMMMMMMMM        
+#   .MMMM'  MMMMMMMMMMMMMMMMMMMMMM                   _____  _    _ ______ _      
+#  MMMMMM    `MMMMMMMMMMMMMMMMMMMM.                 |  __ \| |  | |  ____| |     
+# MMMMMMMM      MMMMMMMMMMMMMMMMMM .                | |__) | |__| | |__  | |     
+# MMMMMMMMM.       `MMMMMMMMMMMMM' MM.              |  _  /|  __  |  __| | |     
+# MMMMMMMMMMM.                     MMMM             | | \ \| |  | | |____| |____ 
+# `MMMMMMMMMMMMM.                 ,MMMMM.           |_|  \_\_|  |_|______|______|     
+#  `MMMMMMMMMMMMMMMMM.          ,MMMMMMMM.
+#     MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM  
+#       MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM:
+#          MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+#             `MMMMMMMMMMMMMMMMMMMMMMMM:
+#                 ``MMMMMMMMMMMMMMMMM'        
+                              
+      
+
+script_dir=$(dirname $(realpath "$0"))
+
+cd $script_dir
+
+echo -e '\n\nInstalling fonts\n\n'
 
 #Installing font
 cd ~/.local/share/fonts && curl -fLO https://github.com/ryanoasis/nerd-fonts/raw/HEAD/patched-fonts/DroidSansMono/DroidSansMNerdFont-Regular.otf
 
-cd $(dirname $0)
+echo -e '\n\nFonts installed!\n\n'
+
+cd $script_dir
+
+echo -e '\n\nInstalling themes\n\n'
 
 # Installing Icon theme
 sudo yum install papirus-icon-theme.noarch
@@ -15,6 +44,9 @@ sudo yum install breeze-cursor-theme.noarch
 
 # Installing eza
 sudo yum install eza
+
+ls
+echo $(pwd)
 
 # Installing Starship
 curl -sS https://starship.rs/install.sh | sh
